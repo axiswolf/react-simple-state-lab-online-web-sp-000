696f37aaf17662909fbb47510cd28f6b15a96afb
Karen V <karenvo@usc.edu>
2020-01-15 10:30:32 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -1,24 +1,40 @@
-import React from 'react';
+// import React from 'react';
+// 
+// export default class Cell extends React.Component {
+//   constructor(props) {
+//       super()
+//       this.state = {
+//           color: props.value
+//       }
+//   }
+// 
+//   updateColor = () =>{
+//       this.setState({
+//           color: '#333'
+//       })
+//   }
+//   render(){
+//       return (
+//           <div className="cell" onClick={this.updateColor} style={{backgroundColor: this.state.color}}>
+//           </div>
+//       )
+//   }
+// 
+// 
+// }
+import React, { Component } from 'react';
 
-export default class Cell extends React.Component {
-  constructor(props) {
-      super()
-      this.state = {
-          color: props.value
+export default class Cell extends Component {
+
+    constructor(props) {
+        super()
+        this.state = {color: '#F00'} 
       }
-  }
 
-  updateColor = () =>{
-      this.setState({
-          color: '#333'
-      })
-  }
-  render(){
-      return (
-          <div className="cell" onClick={this.updateColor} style={{backgroundColor: this.state.color}}>
-          </div>
-      )
-  }
+    render(){
+        return(
+            <div className="cell" style={{backgroundColor: '#F00'}}></div>
+        )
+    }
 
-
-}
+} 
