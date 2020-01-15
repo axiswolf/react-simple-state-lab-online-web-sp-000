b1068ec106dc6ca788cf4a5f545feda942b6be41
Karen V <karenvo@usc.edu>
2020-01-15 10:11:35 +0000

    Automatically backed up by Learn

--- src/Matrix.js
+++ src/Matrix.js
@@ -21,8 +21,8 @@ export default class Matrix extends Component {
 
   defaultProps = {
     values: (() => {
-      const defineRow = ['#F00', '#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00']
+      const defineRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
       return (new Array(10).fill(defineRow))
-    })
+    })()
   }
 }
