3367ed8ab9e3f5ea915e3cfc440ef1aa69209cbe
Karen V <karenvo@usc.edu>
2020-01-15 10:42:21 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class Cell extends React.Component {
   constructor(props) {
-      super()
+      super(props)
       this.state = {
           color: props.value
       }
