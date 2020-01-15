c6840e9f6b15f60001785586822aa486f5599446
Karen V <karenvo@usc.edu>
2020-01-15 10:19:30 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -4,25 +4,13 @@ export default class Cell extends Component {
 
     constructor(props) {
         super()
-        this.state = {
-            color: '#fff'
-        }
-    }
+        this.state = {color: '#F00'} 
+      }
 
-    handleClick = () => {
-        this.setState({
-            color: '#333'
-        })
-    }
-
-    render() {
-        return (
-            <div
-                className="cell"
-                style={{backgroundColor: this.state.color}}
-                onClick={this.handleClick}>
-            </div>
+    render(){
+        return(
+            <div className="cell" style={{backgroundColor: '#F00'}}></div>
         )
     }
 
-}
+} 
