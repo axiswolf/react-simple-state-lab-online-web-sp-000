89ca15d92b144e2f92734cfba720054ef52ca785
Karen V <karenvo@usc.edu>
2020-01-15 10:28:38 +0000

    Automatically backed up by Learn

--- src/Matrix.js
+++ src/Matrix.js
@@ -8,7 +8,7 @@ export default class Matrix extends Component {
   )
 
   genMatrix = () => (
-    this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)})
+    this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   )
 
   render() {
